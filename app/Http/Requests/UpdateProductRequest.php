b4f1de40742b878a461879860b3f9b18f6316517
unknown <ubaydillah.m25@gmail.com>
2026-03-14 09:47:07 +0700

    feat product

---
+++ app/Http/Requests/UpdateProductRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'product_category_id' => 'sometimes|required|exists:product_categories,id',
+            'name' => 'sometimes|required|string|max:255',
+            'price' => 'sometimes|required|numeric|min:0',
+            'stock' => 'sometimes|required|integer|min:0',
+        ];
+    }
+}
